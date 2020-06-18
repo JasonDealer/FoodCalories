89d01e621e1b4722f0c9a041e529abc29ab0efbe
Maksim <makcxx20@gmail.com>
2020-06-18 15:01:23 +0300

    backNotWorking

--- js/script.js
+++ js/script.js
@@ -133,7 +133,7 @@ document.addEventListener('DOMContentLoaded',() => {
 
     //
 
-    //const modalTimerId = setTimeout(openModal, 5000);
+    const modalTimerId = setTimeout(openModal, 5000);
 
     //Проверка на то, что пользователь долистал до конца страницы
     /*if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) */
@@ -212,6 +212,45 @@ document.addEventListener('DOMContentLoaded',() => {
         'menu__item'
         ).render();
     
-    
+    //forms send
+
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка',
+        sucsess: 'Спасибо! С вами свяжутся!',
+        failure: 'Что-то пошло не так'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'multipart/form-data');
+            const formData = new FormData(form);
+
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.sucsess;
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
     
 });
